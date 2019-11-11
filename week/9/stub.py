911e4dd0264397a83ab36bb91f81e6b5eeb115ac
Chris Tharratt <crtharratt@gmail.com>
2019-11-11 21:33:09 +0000

    Completed assignment 9

--- week/9/stub.py
+++ week/9/stub.py
@@ -2,7 +2,7 @@
 
 import sys
 import struct
-
+from datetime import datetime
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
@@ -36,7 +36,122 @@ print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
 
+
+timestamp, = struct.unpack("<l", data[8:12])
+time_readble = datetime.fromtimestamp(timestamp)
+author = struct.unpack("<8s", data[12:20])
+sections = struct.unpack("<L", data[20:24])
+sections = sections[0]
+
+print("TIMESTAMP: %s" % time_readble)
+print("AUTHOR: %s " % author)
+print("SECTION COUNT: %d" % sections)
+print("DATA LENGTH : %d" % len(data))
+
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+offset = 24
+section_count = 0
+
+while (section_count < int(sections)):
+    section_count += 1
+    stype, slen = struct.unpack("<LL", data[offset:offset+8])
+    stype = int(stype)
+    slen = int(slen)
+    #offset += 8
+
+    print("SECTION TYPE: %d" % stype)
+    print("SECTION LENGTH: %d" % slen)
+    
+    
+    #SECTION_ASCII
+    if (stype == 1):
+        ascii = struct.unpack(("<%ds" % slen), data[offset + 8: (offset + 8 + slen)])
+        ascii = ascii[0]
+        print(ascii)
+        
+    #SECTION_UTF8
+    if (stype == 2):
+        utf = struct.unpack(("<%ds" % slen), data[offset + 8: (offset + 8 + slen)])
+        utf = utf[0]
+        utf = utf.decode('utf-8')
+        print(utf)
+        
+    #SECTION_WORDS
+    if (stype == 3):
+        word_number = int(slen/4)
+        unpack = "<" + ("%s" % 'L' * word_number)
+        words = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        words = words[0]
+        print(words)
+    
+    #SECTION_DWORDS
+    if (stype == 4):
+        word_number = int(slen/8)
+        unpack = "<" + ("%s" % 'L' * word_number)
+        words = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        words = words[0]
+        print(words)
+        
+    #SECTION_DOUBLES
+    if (stype == 5):
+        word_number = int(slen/8)
+        unpack = "<" + ("%s" % 'd' * word_number)
+        words = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        words = words[0]
+        print(words)
+        
+    #SECTION_COORD
+    if (stype == 6):
+        data_result = struct.unpack("<dd", data[offset + 8: (offset + 8 + slen)])
+        x = data_result[0]
+        y = data_result[1]
+        print("Coordinates: (" + str(x) + ", " + str(y) + ")")
+        
+    #SECTION_REFERENCE 
+    #if (stype == 7):
+    # svalue?
+       
+    #SECTION_PNG
+    if (stype == 8):
+        unpack = "<" + ("%s" % 'B' * (slen))
+        its_magic_you_know = [137, 80, 78, 71, 13, 10, 26, 10]
+        #its_magic_you_know = 0x89504E470D0A1A0A
+        png_out = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        f = open('the_png.png', 'wb')
+        #f.write(str(its_magic_you_know))
+        f.write (bytearray(its_magic_you_know))
+        f.write(bytearray(png_out))
+        print("Png created")
+        
+    #SECTION_GIF87
+    if (stype == 9):
+        unpack = "<" + ("%s" % 'B' * (slen))
+        its_magic_you_know = [47, 49, 46, 38, 37, 61]
+        gif_out = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        f = open('the_gif87.gif', 'wb')
+        f.write(bytearray(its_magic_you_know))
+        f.write(bytearray(gif_out))
+        print("Gif87 created")
+    
+    #SECTION_GIF89
+    if (stype == 10):
+        unpack = "<" + ("%s" % 'B' * (slen))
+        its_magic_you_know = [47, 49, 46, 38, 39, 61]
+        gif_out = struct.unpack(unpack, data[offset + 8: (offset + 8 + slen)])
+        f = open('the_gif89.gif', 'wb')
+        f.write(bytearray(its_magic_you_know))
+        f.write(bytearray(gif_out))
+        print("Gif89 created")
+  
+    offset = offset + slen + 8
+
+
+
+
+
+
+
+
--- week/9/writeup/README.md
+++ week/9/writeup/README.md
@@ -1,15 +1,72 @@
 # Writeup 9 - Forensics II
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Chris Tharratt
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Chris Tharratt
 
 
 ## Assignment details
 
 ### Part 1 (45 Pts)
 
+1. Warmup: what IP address has been attacked?
+
+    `142.93.136.81`
+
+2. What kind of assessment tool(s) were the attackers using against the victim machine? List the name(s) of the tool(s) as well.
+
+    The attackers launched a port scanning attack after connecting to server's website on port 80. The attacker keeps sending Syn and waits until the server responds with Syn Ack, but never completes the handshake with an Ack. The attacking IP continues this for different ports. This is likely using `nmap` or another similar port scanning tool. 
+
+3. What are the hackers' IP addresses, and where are they connecting from?
+
+    `159.203.113.181`. Through a ip geo lookup, the IP is based in Clifton, New Jersey
+
+4. What port are they using to steal files on the server?
+
+    The attackers are using port 21 (ftp)
+
+5. Which file did they steal? What kind of file is it? Do you recognize the file?
+
+    The stole the file `find_me.jpeg`. It is a jpeg image file. 
+
+6. Which file did the attackers leave behind on the server?
+
+    The attackers left behind `greetz.fpff`.
+
+7. What is a countermeasure to prevent this kind of intrusion from happening again? Note: disabling the vulnerable service is *not* an option.
+
+    They should be able to implement protections against port scanning by blacklisting ip's that continue to not complete a tcp handshake across multiple ports. This should rapidly slow down their reconasance. Additionally, using a more secure service than FTP and a more secure password than `linkinpark` would be beneficial. 
+
 ### Part 2 (55 Pts)
+
+1. When was `greetz.fpff` generated?
+    
+    The file was generated `2019-03-27 04:15:05`
+    
+2. Who authored `greetz.fpff`?
+
+    The author was `fl1nch`
+    
+4. List each section, giving us the data in it *and* its type.
+
+    SECTION TYPE: 1 => SECTION_ASCII
+    Hey you, keep looking :)
+    SECTION TYPE: 6 => SECTION_COORD
+    Coordinates: (52.336035, 4.880673)
+    SECTION TYPE: 8 => SECTION_PNG
+    png file
+    SECTION TYPE: 1 => SECTION_ASCII
+    }R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC
+    SECTION TYPE: 1 => SECTION_ASCII
+    Q01TQzM4OVIte2hleV9oM3lfeTBVX3lvdV9JX2RvbnRfbGlrZV95b3VyX2Jhc2U2NF9lbmNvZGluZ30=
+
+5. Report *at least* one flag hidden in `greetz.fpff`. Any other flag found will count as bonus points towards the *competition* portion of the syllabus.
+
+| __Flag__  | __How it was found__ |
+| ------------- | ------------- |
+|  CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}| Reversing string |
+|  CMSC389R-{hey_h3y_y0U_you_I_dont_like_your_base64_encoding} | Base64 decoding |
+|  CMSC389R-{w3lc0me_b@ck_fr0m_spr1ng_br3ak} | Png file |
